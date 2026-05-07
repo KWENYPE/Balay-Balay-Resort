fc59f8b1db6c94bca4fb85b2c629e61bfa27cbac
unknown <markdanielfernandez11@gmail.com>
2026-05-07 11:47:49 +0800

    complete system: needed QA testing

---
+++ Balay-Balay-Resort/Models/Property.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Balay_Balay_Resort.Models
+{
+    public class Property
+    {
+        [Key]
+        public int Property_ID { get; set; }
+
+        public string Property_Name { get; set; }
+
+        public int BedNum { get; set; }
+
+        public int BathNum { get; set; }
+
+        public int GuestCapacity { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string AmountUnit { get; set; }
+
+        public int UnitNumber { get; set; }
+
+        public string PropertyImagePath { get; set; }
+
+        public ICollection<Amenity_Property> Amenity_Properties { get; set; } = new List<Amenity_Property>();
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+        public ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+    }
+}
---
+++ Balay-Balay-Resort/ViewModels/AdminPropertiesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Balay_Balay_Resort.Models;
+
+namespace Balay_Balay_Resort.ViewModels;
+public class AdminPropertiesViewModel
+{
+    public List<Property> Properties { get; set; } = new();
+    public string SearchQuery { get; set; } = "";
+    public Property NewProperty { get; set; } = new();
+}
