b48c06c43f53f179accc31ed430c7f30d65a4bb5
KWENYPE <karl2003pezwen19@gmail.com>
2026-05-07 19:11:30 +0800

    fix: remove duplicate files and subfolder causing build errors

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BalayBalayResort.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Models/MyBookingsViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BalayBalayResort.Models;
-
-public class MyBookingsViewModel
-{
-    public List<BookingModel> Bookings { get; set; } = new();
-    public List<TransactionModel> Transactions { get; set; } = new();
-    public string SearchQuery { get; set; } = "";
-}
